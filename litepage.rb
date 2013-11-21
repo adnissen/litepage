641ccc3e1221a134164a9c2f17ca02e6fa6fdc3f
adnissen <andrew_nissen@yahoo.com>
2013-11-20 20:43:06 -0500

    messing around with more db stuff

--- litepage.rb
+++ litepage.rb
@@ -3,25 +3,26 @@ require 'redcarpet'
 require 'mongo'
 require 'uri'
 
-configure do
-	def get_connection
-	  return @db_connection if @db_connection
-	  db = URI.parse(ENV['MONGOHQ_URL'])
-	  db_name = db.path.gsub(/^\//, '')
-	  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-	  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-	  @db_connection
-	end
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
 
-	db = get_connection
+db = get_connection
 
-	puts "Collections"
-	puts "==========="
-	collections = db.collection_names
-	puts collections
+get '/' do
+  puts "Collections"
+  puts "==========="
+  collections = db.collection_names
+  puts collections
+  puts
 end
 
 post '/makePage' do
-	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
-	puts params[:markdown]
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+  puts params[:markdown]
 end
